b5307afef594359b821dc3d194fbedc233937404
kongnote <kongnote9@gmail.com>
2018-07-16 23:58:14 +0900

    start

---
+++ src/main/java/com/cocktail_web/CocktailWebApplication.java
@@ -0,0 +1,12 @@
+package com.cocktail_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailWebApplication.class, args);
+	}
+}
